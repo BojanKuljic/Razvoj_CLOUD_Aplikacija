cd329c4e2e5559b8547ef954a0598bf45a39f10a
dragangavric01 <dragangavric001@gmail.com>
2024-06-04 15:35:26 +0200

    Implementirano slanje mejlova

---
+++ EmailSending/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+
+namespace EmailSending {
+    /*
+        How to set up a google account so it can be used by a third party app:
+            Step one: enable 2FA
+                https://myaccount.google.com/signinoptions/two-step-verification/enroll-welcome
+            Step two: create an app - specific password
+                https://myaccount.google.com/apppasswords
+            After this, put the sixteen-letter password in Authenticate() method of SmtpClient
+    */
+    public class EmailSender {
+        string senderAddress = "throwaway134322312@gmail.com";
+        string senderAppPassword = "veqw ptoq qgmb ulwo";
+
+        string smtpServerAddress = "smtp.gmail.com";
+        int smtpServerPortNumber = 587;
+
+        // example: Send("receiver@gmail.com", "Example", "This is an example")
+        public bool Send(string receiverAddress, string emailSubject, string emailBody) {
+            try {
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(senderAddress));
+                email.To.Add(MailboxAddress.Parse(receiverAddress));
+                email.Subject = emailSubject;
+                email.Body = new TextPart(TextFormat.Plain) { Text = emailBody };
+
+                using (var smtp = new SmtpClient()) {
+                    smtp.Connect(smtpServerAddress, smtpServerPortNumber, SecureSocketOptions.StartTls);
+                    smtp.Authenticate(senderAddress, senderAppPassword);
+                    smtp.Send(email);
+                    smtp.Disconnect(true);
+                }
+
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+}
